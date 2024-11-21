1249c3003fbde6ed429bd7d4eae0855f7e7932d3
agusttin-ader <agusttin.ader@hotmail.com>
2024-11-20 22:34:47 -0300

    clase3

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import NavBar from './components/NavBar'
+
+// cntrl C para salir de npm run dev muchas veces, y npm run dev para iniciarlo.
+
+
+// import { NavBar, saludo } from './components/NavBar'
+
+
 
 function App() {
+  const CONDICION = false
+  const ESTILOS_H1 = {color: CONDICION === true ? "purple" : "red",  fontSize: "50px"}
 
   return (
     <div>
-      <h1>Hola mundo!</h1>
+      <NavBar />
+      <h1 style={ESTILOS_H1}>Hola mundo!</h1>
+      <img /> 
     </div>
   )
 }
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,12 @@
+const Banner = () => {
+
+    return (
+        <div>
+            <img src="https://img.freepik.com/vector-gratis/plantilla-banner-horizontal-plano-rebajas-viernes-negro_23-2150852978.jpg?semt=ais_hybrid" alt="" />
+        </div>
+
+    )
+
+}
+
+export default Banner; //Se exporta por defecto el componente Banner
